16eb020f7926084fa2559d4bcf6749ccc20661b5
Caenepeel Matthias <matthias.caenepeel@telenetgroup.be>
2018-08-27 18:55:01 +0200

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+const {ObjectID} = require("mongodb");
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
@@ -32,6 +33,25 @@ app.get("/todos", (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+           return  res.status(404).send();
+        }
+        
+        res.send({todo});
+        
+       }).catch((e) => {
+        res.status(400).send(); 
+       })
 });
 
 app.listen(3000, () => {
